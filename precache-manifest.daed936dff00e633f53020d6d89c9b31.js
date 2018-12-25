191a55e1411989a47c50b5258bbc2d5e15fba156
Max Melnychuk <max.melnychuk1@gmail.com>
2018-12-25 14:20:35 +0200

    Updates

---
+++ precache-manifest.daed936dff00e633f53020d6d89c9b31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcfd8ea4922752ef2dc8acbc659616d0",
+    "url": "/weather_app_lenal/static/media/SpinnerGear.fcfd8ea4.svg"
+  },
+  {
+    "revision": "6fbb61555f838601f30d",
+    "url": "/weather_app_lenal/static/js/runtime~main.6fbb6155.js"
+  },
+  {
+    "revision": "f9d6ec6e60dcddffb016",
+    "url": "/weather_app_lenal/static/js/main.f9d6ec6e.chunk.js"
+  },
+  {
+    "revision": "02862c6d1079616e9689",
+    "url": "/weather_app_lenal/static/js/1.02862c6d.chunk.js"
+  },
+  {
+    "revision": "f9d6ec6e60dcddffb016",
+    "url": "/weather_app_lenal/static/css/main.12ea5404.chunk.css"
+  },
+  {
+    "revision": "eca19154bddf8e80611c498ef27e7e35",
+    "url": "/weather_app_lenal/index.html"
+  }
+];
